5f609ccedfd4c98d0ee4494f294b36bcc61373e5
Sezer Kulekci <sezerklkci@gmail.com>
2023-11-15 16:03:16 +0300

    new challenge

---
+++ 05-challenge/index.js
@@ -0,0 +1,43 @@
+// Challenge 5
+//
+// Description:
+// 1-100 arasinda rastgele uretilen 10 sayidan en kucugu ile en buyugunu ve aritmetik ortalamayi bulan program.
+//
+// Example Output:
+// Uretilen sayilar: [10,9,34,9,15,24,7]
+// En kucuk sayi: [7]
+// En buyuk sayi: [34]
+// Aritmetik ortalama: [21]
+
+//DEĞİŞKENLER
+let yeniDizi = [];
+let enKucuk = undefined;
+let enBuyuk = undefined;
+let toplam = 0;
+ortalama = undefined;
+//RASTGELE SAYI
+for (let sayi = 0; sayi < 10; sayi++) {
+  yeniDizi.push(Math.floor(Math.random() * 101));
+}
+
+yeniDizi.forEach((x) => {
+  //EN KÜÇÜK SAYI
+  if (enKucuk == undefined) {
+    enKucuk = x;
+  } else if (x < enKucuk) {
+    enKucuk = x;
+  }
+  if (enBuyuk == undefined) {
+    enBuyuk = x;
+  } else if (x > enBuyuk) {
+    enBuyuk = x;
+  }
+  toplam += x;
+});
+
+ortalama = toplam / yeniDizi.length;
+
+console.log(`1-100 arası rastgele 10 sayı : ${yeniDizi}`);
+console.log(`Aralarındaki en küçük sayı : ${enKucuk}`);
+console.log(`Aralarındaki en büyük sayı : ${enBuyuk}`);
+console.log(`Sayıların Aritmetik ortalaması ${ortalama}`);
