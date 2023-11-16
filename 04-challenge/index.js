410f9620ff5a74e2025f91bf4b0360a12e2dba2f
Sezer Kulekci <sezerklkci@gmail.com>
2023-11-16 11:47:31 +0300

    update code

--- 04-challenge/index.js
+++ 04-challenge/index.js
@@ -7,6 +7,17 @@
 // Verilen dizi: [0,9,4,9,5,4,7,0,5,4]
 // Benzersiz dizi: [0,9,4,5,7]
 
+// let verilenDizi = [0, 9, 4, 9, 5, 4, 7, 0, 5, 4];
+// let sade = [];
+// verilenDizi.forEach((sayi) => {
+//   const varMi = sade.indexOf(sayi) == -1;
+//   if (varMi == false) {
+//   } else {
+//     sade.push(sayi);
+//   }
+// });
+// console.log(sade);
+
 let verilenDizi = [0, 9, 4, 9, 5, 4, 7, 0, 5, 4];
 let sade = [];
 
